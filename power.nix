efb95ad135ad789df5f7173c66ba370aeeebe2e3
Charles Liu <charles2560710@gmail.com>
2025-08-28 18:14:47 +1000

    Remove some power options

--- power.nix
+++ power.nix
@@ -10,9 +10,4 @@
     # kill user's processes at logout, false by default
     killUserProcesses = false;
   };
-  # Disable system default power management
-  systemd.targets.sleep.enable = true;
-  systemd.targets.suspend.enable = true;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = true;
 }
