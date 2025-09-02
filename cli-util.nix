b6e8d05b64d417c3ccb4adf40450521868a85639
Charles Liu <charles2560710@gmail.com>
2025-09-02 19:34:18 +1000

    Add tmux and configs (tested)

--- cli-util.nix
+++ cli-util.nix
@@ -1,8 +1,7 @@
 { config, pkgs, ... }:
-# CLI utility
+# CLI Visual
 {
   environment.systemPackages = with pkgs; [
-    # CLI Visual
     bat
     eza
     jq
@@ -12,4 +11,24 @@
     wl-clipboard  # clipboard provider for nvim
     neovim
   ];
+  programs.tmux = {
+    enable = true;
+    terminal = "screen-256color";
+    plugins = with pkgs.tmuxPlugins; [
+      nord
+      # tmux-floax
+    ];
+    extraConfigBeforePlugins = ''
+      # true colour
+      set -ga terminal-overrides ",*-256color:Tc"
+    '';
+    extraConfig = ''
+      # open panes from cwd
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind '%' split-window -h -c "#{pane_current_path}"
+
+      # open windows from cwd
+      bind c new-window -c "#{pane_current_path}"
+    '';
+  };
 }
