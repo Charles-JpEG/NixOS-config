e3ee13e7a4402225d93d929723ed6cab0a1e3aad
Charles Liu <charles2560710@gmail.com>
2025-09-21 00:30:33 +1000

    Update firewall config ( sync currently deployed ver )

--- firewall.nix
+++ firewall.nix
@@ -4,7 +4,7 @@
 {
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 22 233 4444 3200 7844 ];
+    allowedTCPPorts = [ 22 4444 3200 7844 ];
     allowedUDPPorts = [ 7844 ];
   };
 }
