3b1c01265ff014822dd1b20b275a0066d601f5c5
Charles Liu <charles2560710@gmail.com>
2025-09-20 15:02:45 +1000

    Switch back to pipewire

--- audio.nix
+++ audio.nix
@@ -8,32 +8,31 @@
     sof-firmware
   ];
 
-  # Enable sound with pulseaudio
-  services.pulseaudio = {
-    enable = true;
-    support32Bit = true;
-    package = pkgs.pulseaudioFull;
-  };
-
-  services.pipewire.enable = lib.mkForce false;
-  # services.pipewire = {
-  #   enable = false;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-  #   # If you want to use JACK applications, uncomment this
-  #   #jack.enable = true;
-  #
-  #   # use the example session manager (no others are packaged yet so this is enabled by default,
-  #   # no need to redefine it in your config for now)
-  #   #media-session.enable = true;
+  # services.pulseaudio = {
+  #   enable = true;
+  #   support32Bit = true;
+  #   package = pkgs.pulseaudioFull;
   # };
+
+  # Enable sound with pipewire
+  # services.pipewire.enable = lib.mkForce false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
   security.rtkit.enable = true;
 
   environment.systemPackages = with pkgs; [
     alsa-lib
     alsa-plugins
     alsa-utils
-    pavucontrol
   ];
 }
