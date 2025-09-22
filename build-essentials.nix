4a1c8f88a04f69057138ec47dfb4c4cb250fddde
Charles Liu <charles2560710@gmail.com>
2025-09-22 21:19:56 +1000

    Update: build/test packages and [ zip file ]

--- build-essentials.nix
+++ build-essentials.nix
@@ -3,9 +3,14 @@
 {
   environment.systemPackages = with pkgs; [
     gcc
+    gdb
     cmake
     ninja
     # Extra
+    cling
+    lldb_21
+    llvmPackages_21.clang
+    llvmPackages_21.libcxx
     libgcc
     pkg-config
     rustup
--- configuration.nix
+++ configuration.nix
@@ -85,7 +85,9 @@
     vim
     wget
     git
+    zip
     unzip
+    file
   ] ++ [
     docker
     qemu
