ba5b077f5d82c7717b4e31471135224b4846c603
Charles Liu <charles2560710@gmail.com>
2025-09-23 15:26:38 +1000

    Add virtualbox and zed

--- gui.nix
+++ gui.nix
@@ -5,6 +5,7 @@
     google-chrome
     # IDE
     vscode
+    zed-editor-fhs
     jetbrains-toolbox
     jetbrains.webstorm
     jetbrains.clion
@@ -14,8 +15,16 @@
     alacritty
     # social
     telegram-desktop
+    # vbox module
+    linuxKernel.packages.linux_6_16.virtualbox
   ];
   # Install firefox
   programs.firefox.enable = true;
   programs.steam.enable = true;
+  # VM
+  # virtualbox
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  users.extraGroups.vboxusers.members = [ "charles" ];
+  boot.blacklistedKernelModules = [ "kvm" "kvm_intel" ];
 }
